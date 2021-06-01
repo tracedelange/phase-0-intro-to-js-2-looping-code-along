e6f321f9f31ea79102e1a95a662ecfcc5583cc02
Trace <tracedelange@me.com>
2021-06-01 14:30:44 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+
+}
+
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+    return gifts
+}
+
+wrapGifts(gifts)
+
+function writeCards(names, event) {
+    const thankYous = []
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        thankYous.push(message)
+    }
+    return thankYous
+}
+
+function countDown(n) {
+    while (n > -1) {
+        console.log(n)
+        n--
+    }
+}
